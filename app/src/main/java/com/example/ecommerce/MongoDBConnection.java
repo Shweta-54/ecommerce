f2f6fb12ce66f3bfe63a02a8f48e606841d51696
Shweta-54 <shwetababy379@gmail.com>
2025-02-05 14:50:57 +0530

    from shweta

---
+++ app/src/main/java/com/example/ecommerce/MongoDBConnection.java
@@ -0,0 +1,9 @@
+package com.example.ecommerce;
+
+
+
+public class MongoDBConnection {
+
+
+}
+    //mongodb+srv://shwetasharma:<db_password>@ecommerce.7zupo.mongodb.net/?retryWrites=true&w=majority&appName=Ecommerce
