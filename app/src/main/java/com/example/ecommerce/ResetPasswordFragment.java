3a30d8f66d73bf24412317bf7277d0cf6c4bfb66
Shweta-54 <shwetababy379@gmail.com>
2025-02-23 14:39:39 +0530

    from shweta

--- app/src/main/java/com/example/ecommerce/ResetPasswordFragment.java
+++ app/src/main/java/com/example/ecommerce/ResetPasswordFragment.java
@@ -3,13 +3,6 @@ package com.example.ecommerce;
 import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.TransitionRes;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
@@ -25,9 +18,13 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 
 /**
