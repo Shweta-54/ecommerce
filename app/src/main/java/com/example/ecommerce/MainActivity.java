68e76650c0488bdc9796aab0cb94882ad0667652
usha-2214 <ushakushwah2005@gmail.com>
2025-03-04 00:09:01 +0530

    from usha

--- app/src/main/java/com/example/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -40,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int REWARDS_FRAGMENT = 4;
 
+    private static final int ACCOUNT_FRAGMENT = 5;
+
+
     private Window window;
     private Toolbar toolbar;
 
@@ -137,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
         } else if (id == R.id.nav_my_wishlist) {
 
         } else if (id == R.id.nav_my_account) {
-
+            gotoFragment("My Account",new MyAccountFragment(),ACCOUNT_FRAGMENT);
         }else if (id == R.id.nav_sign_out){
 
         }
