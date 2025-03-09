74f9b4452023248146b3309d3f3e4101f78ab5cf
usha-2214 <ushakushwah2005@gmail.com>
2025-03-09 18:36:36 +0530

    from usha

--- app/src/main/java/com/example/ecommerce/CategoryAdapter.java
+++ app/src/main/java/com/example/ecommerce/CategoryAdapter.java
@@ -55,11 +55,11 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.viewHo
             categoryName = itemView.findViewById(R.id.category_name);
         }
 
-        private  void setCategoryIcon(String iconUrl){
-            if (!iconUrl.equals("null"))
-            Glide.with(itemView.getContext()).load(iconUrl).apply(new RequestOptions().placeholder(R.drawable.baseline_home_24)).into(categoryIcon);
+        private  void setCategoryIcon(String iconUrl) {
+            if (!iconUrl.equals("null")) {
+                Glide.with(itemView.getContext()).load(iconUrl).apply(new RequestOptions().placeholder(R.drawable.baseline_home_24)).into(categoryIcon);
+            }
         }
-
         private void setCategory(final String name, final int position){
             categoryName.setText(name);
             itemView.setOnClickListener(new View.OnClickListener() {
