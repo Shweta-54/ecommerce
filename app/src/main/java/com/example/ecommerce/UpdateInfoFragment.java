5138d4cab0fd03e1a7b8c56328aad00d0c86b7b2
Shweta-54 <shwetababy379@gmail.com>
2025-04-06 11:31:33 +0530

    from shweta

---
+++ app/src/main/java/com/example/ecommerce/UpdateInfoFragment.java
@@ -0,0 +1,26 @@
+package com.example.ecommerce;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class UpdateInfoFragment extends Fragment {
+
+
+
+    public UpdateInfoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update_info, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/ecommerce/UpdatePasswordFragment.java
@@ -0,0 +1,24 @@
+package com.example.ecommerce;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class UpdatePasswordFragment extends Fragment {
+
+    public UpdatePasswordFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update_password, container, false);
+    }
+}
